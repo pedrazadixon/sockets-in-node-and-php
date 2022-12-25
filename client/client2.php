4bfc01309dd0b8a87b6d61e8e3c997a1610f9313
Dixon Pedraza <pedrazadixon@gmail.com>
2022-12-25 16:55:05 -0500

    print response in client 2

--- client/client2.php
+++ client/client2.php
@@ -4,10 +4,15 @@ require 'vendor/autoload.php';
 
 use ElephantIO\Client;
 
+session_start();
+
 $url = 'http://localhost:3000';
 $client = new Client(Client::engine(Client::CLIENT_4X, $url));
 $client->initialize();
 $client->of('/');
 
-$data = ['username' => 'my-user'];
 $client->emit('message', ['ping']);
+
+if ($packet = $client->wait('message')) {
+    echo $packet->data;
+}
